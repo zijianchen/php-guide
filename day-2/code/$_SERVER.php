f9ed7596e26896e957b992291311a81d49aff58c
max <chenzjmax@gmail.com>
2019-03-08 14:03:48 +0800

    add day 2 notes

---
+++ day-2/code/$_SERVER.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Administrator
+ * Date: 2019/3/08
+ * Time: 14:53
+ */
+echo '<pre>';
+var_dump($_SERVER);
+
+?>
+
+<script>
+    document.write(navigator.userAgent);
+</script>
